dccc4c215aa908fae482f4341628cc0ad4e4b62f
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-02-25 07:49:52 +0100

    Add referral and review routes to the main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import authRoute from './auth.routes';
 import userRoute from './user.routes';
+import referralRoutes from './referral.routes';
+import reviewRoutes from './reviews.routes';
 // import AdminRoutes from './Admin/admin.routes';
-
 
 const router = Router();
 
 router
+    .use('/referral', referralRoutes)
+    .use('/review', reviewRoutes)
     .use('/auth', authRoute)
     // .use('/iamBase', adminRoute)
     .use('/user', userRoute);
