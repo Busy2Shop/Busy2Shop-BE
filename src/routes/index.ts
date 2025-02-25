612078f5e725fc63a988a9a752bf41c73e7e18e3
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-02-25 17:28:27 +0100

    Refactor routes to organize and include market, category, product, shopping list, order, and vendor routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,29 @@
 import { Router } from 'express';
 import authRoute from './auth.routes';
 import userRoute from './user.routes';
-import referralRoutes from './referral.routes';
+import referralRoute from './referral.routes';
 import reviewRoutes from './reviews.routes';
+import marketRoute from './market.routes';
+import categoryRoute from './category.routes';
+import productRoute from './product.routes';
+import shoppingListRoute from './shoppingList.routes';
+import orderRoute from './order.routes';
+import vendorRoute from './vendor.routes';
 // import AdminRoutes from './Admin/admin.routes';
 
 const router = Router();
 
-router
-    .use('/referral', referralRoutes)
-    .use('/review', reviewRoutes)
-    .use('/auth', authRoute)
-    // .use('/iamBase', adminRoute)
-    .use('/user', userRoute);
+router.use('/auth', authRoute);
+router.use('/user', userRoute);
+router.use('/market', marketRoute);
+router.use('/category', categoryRoute);
+router.use('/product', productRoute);
+router.use('/shopping-list', shoppingListRoute);
+router.use('/order', orderRoute);
+router.use('/vendor', vendorRoute);
+router.use('/review', reviewRoutes);
+router.use('/referral', referralRoute);
+// router.use('/admin', AdminRoutes);
 
 export default router;
 
