d5c63d7d3aa320d4a3cd958fef5b2ef61dfd437c
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2025-04-19 14:36:50 +0100

    Update email sender name and simplify HTML nullish check
    Changed the sender name in email configurations from "Base Accounts" to "Busy2Shop Accounts" to reflect branding. Simplified the nullish check for the HTML property using the nullish coalescing operator.

--- src/utils/Email/index.ts
+++ src/utils/Email/index.ts
@@ -92,10 +92,10 @@ export default class EmailService {
                 // Use Promise.all to wait for all emails to send
                 await Promise.all((options.postmarkInfo ?? []).map(async (recipient) => {
                     const mailOptions = {
-                        from: `Base Accounts<${EMAIL_SERVICE === 'zoho' ? ZOHO_USERNAME : EMAIL_HOST_ADDRESS}>`,
+                        from: `Busy2Shop Accounts<${EMAIL_SERVICE === 'zoho' ? ZOHO_USERNAME : EMAIL_HOST_ADDRESS}>`,
                         to: recipient.recipientEmail,
                         subject: options.subject,
-                        html: options.html ? options.html : undefined,
+                        html: options.html ?? undefined,
                         attachments: options.attachments,
                     };
 
