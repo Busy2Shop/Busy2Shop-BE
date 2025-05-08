fd06ca1d087d32e1119ab8baea62601270dd7438
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-05-08 15:19:21 +0100

    fix: Update ESLint rules to allow Prettier to handle indentation and adjust unused variable settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,21 @@ module.exports = {
         node: true,
     },
     rules: {
-        indent: ['error', 4],
+        // 'prettier/prettier': 'error',
+        indent: 'off', // Let Prettier handle indentation
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
         'comma-dangle': ['error', 'always-multiline'],
-        'object-curly-spacing': ['error', 'always'],
         'no-var': 'error',
-        'no-unused-vars': 'warn',
+        'no-unused-vars': 'off', // Use @typescript-eslint/no-unused-vars instead
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            },
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
         'no-undef': 'error',
         'no-empty-function': 'error',
         'no-unescaped-entities': 'off',
